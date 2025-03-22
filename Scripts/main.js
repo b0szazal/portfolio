3f526c89c462176dcc30cf066e31f180ee6a7f8a
b0szazal <b0szazal@vasvari.org>
2025-03-22 15:22:05 +0100

    minor toast changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,6 +5,7 @@ if(email){
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
     email.addEventListener("click", ()=>{
         navigator.clipboard.writeText("zalan.szasz06@gmail.com");
+        document.getElementById("toastMessage").innerHTML="Email address copied to clipboard"
         toastBootstrap.show();
     })
 }
